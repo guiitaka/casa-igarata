99ed45c62407fe696c61d0931735c50fde0211f6
Guilherme Takahashi <guilhermetakahashi@Guilhermes-MacBook-Air.local>
2024-11-05 22:39:03 -0300

    Update next config for large files

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'placehold.co'],
     unoptimized: true,
   },
+  // Adicione esta configuração para arquivos grandes
+  webpack: (config) => {
+    config.performance.maxAssetSize = 1024 * 1024 * 100; // 100MB
+    config.performance.maxEntrypointSize = 1024 * 1024 * 100; // 100MB
+    return config;
+  },
 }
 
 module.exports = nextConfig 
